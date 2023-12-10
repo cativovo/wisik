4526363ba993cc3fd855113f3e02e5b94274c939
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-12-10 18:57:09 +0800

    setup tailwind

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -21,10 +21,21 @@ func NewServer(is *image.Service) *Server {
 	s.router.Use(middleware.Logger)
 
 	// pages/handlers
+	s.serveStaticFiles()
 	s.registerPages(is)
 	s.registerHandlers(is)
 
 	return s
+}
+
+func (s *Server) serveStaticFiles() {
+	dir := "public"
+	path := "/" + dir
+
+	s.router.Get(path+"/*", func(w http.ResponseWriter, r *http.Request) {
+		fs := http.StripPrefix(path, http.FileServer(http.Dir(dir)))
+		fs.ServeHTTP(w, r)
+	})
 }
 
 func (s *Server) ListenAndServe(addr string) {
